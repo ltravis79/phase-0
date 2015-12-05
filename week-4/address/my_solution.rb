3134aa86a73c4152be3ed1f427af163670b1b779
Lindsey Ullman <lindsey.ullman@gmail.com>
2015-12-05 02:45:10 -0700

    Add files for exercises and reflection

---
+++ week-4/address/my_solution.rb
@@ -0,0 +1,12 @@
+# Format an Address
+
+# I worked on this challenge by myself.
+
+
+# Your Solution Below
+
+def make_address(street, city, state, zip)
+	return 'You live at ' + street + ', in the beautiful city of ' + city + ', ' + state + '. Your zip is ' + zip + '.'
+end
+
+p make_address("123 Main St.", "Phoenix", "AZ", "85028")
