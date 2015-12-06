7be38706498d00e8fa49e041678bab959ccab737
Lindsey Ullman <lindsey.ullman@gmail.com>
2015-12-06 09:57:49 -0700

    Update factorial solution.

--- week-4/factorials/my_solution.rb
+++ week-4/factorials/my_solution.rb
@@ -1,9 +1,23 @@
 # Factorial
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge with Dan Turcza.
 
 
 # Your Solution Below
 def factorial(number)
   # Your code goes here
+  # PSEUDOCODE
+  # Initialize a variable for the product as 1
+  # Loop with an increment
+  # => Multiply product by increment
+  # Return product
+
+  # initial solution
+  product = 1
+
+  for increment in (1..number)
+    product = product * increment
+  end
+
+  return product
 end
